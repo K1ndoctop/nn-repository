6df7f0cd862f6bab191610dc805a04bf71da8153
kjbuhyvbhxhcbiusgrelgmkef <malik.vundiz@gmail.com>
2023-10-23 19:51:22 +0600

    k

--- src/store/users/usersSLice.js
+++ src/store/users/usersSLice.js
@@ -1,29 +1,28 @@
-// import {createSlice} from '@reduxjs/toolkit'
-// import { getUsers } from './usersActions'
+import { createSlice } from "@reduxjs/toolkit";
+import { getUsers } from "./usersActions";
 
-// const usersSlice = createSlice({
-//     name: 'users',
-//     initialState : {
-//         users: '',
-//         loading: false,
-//         error: '',
-//         oneUser: ''
-//     },
-//     reducers: {
-//     },
-//     extraReducers: (builder) => {
-//         builder
-//             .addCase(getUsers.pending, (state) =>{
-//                 state.loading = true
-//             })
-//             .addCase(getUsers.fulfilled, (state, action) => {
-//                 state.users = action.payload
-//                 state.loading = false
-//             })
-//             .addCase(getUsers.rejected, (state) => {
-//                 state.error = true
-//             })
-//     }
-// })
+const usersSlice = createSlice({
+  name: "users",
+  initialState: {
+    users: "",
+    loading: false,
+    error: "",
+    oneUser: "",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.users = action.payload;
+        state.loading = false;
+      })
+      .addCase(getUsers.rejected, (state) => {
+        state.error = true;
+      });
+  },
+});
 
-// export default usersSlice.reducer
+export default usersSlice.reducer;
